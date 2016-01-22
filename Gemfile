fdbf12b7cad56da03bba74777e45fb66b96a1fa9
elifoster <elifosterwy@gmail.com>
2016-01-22 01:28:21 -0800

    :arrow_up: :broken_heart: :fu: :racehorse: Bump ruby version to 2.3.0 finally. Remove stupid sentences thing and use Array#sample as I should.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'http://rubygems.org'
 
-ruby '2.2.3'
+ruby '2.3.0'
 gem 'sinatra', '1.4.6'
--- main.rb
+++ main.rb
@@ -1,27 +1,27 @@
 require 'sinatra'
-require_relative 'lib/drama/sentences'
+require_relative 'lib/drama/constants'
 
 helpers do
   def generate_sentence
     # Splitting into an array is required for easily preventing duplicate vals
-    sent = Drama::Sentences.sentence.split(/\s/)
+    sent = Drama::Constants::SENTENCES.sample.split(/\s/)
     sent.each do |w|
       num = sent.index(w)
-      w = w.gsub('[things]', Drama::Sentences.thing)
-      w = w.gsub('[sites]', Drama::Sentences.site)
-      w = w.gsub('[people]', Drama::Sentences.person)
-      w = w.gsub('[says]', Drama::Sentences.says)
-      w = w.gsub('[badsoft]', Drama::Sentences.badsoft)
-      w = w.gsub('[function]', Drama::Sentences.function)
-      w = w.gsub('[adj]', Drama::Sentences.adjective)
-      w = w.gsub('[enormous]', Drama::Sentences.size)
-      w = w.gsub('[price]', Drama::Sentences.price)
-      w = w.gsub('[ac1]', Drama::Sentences.badverb)
-      w = w.gsub('[packs]', Drama::Sentences.pack)
-      w = w.gsub('[drama]', Drama::Sentences.drama)
-      w = w.gsub('[code]', Drama::Sentences.code)
-      w = w.gsub('[ban]', Drama::Sentences.ban)
-      w = w.gsub('[activates]', Drama::Sentences.activation)
+      w = w.gsub('[things]', Drama::Constants::THINGS.sample)
+      w = w.gsub('[sites]', Drama::Constants::SITES.sample)
+      w = w.gsub('[people]', Drama::Constants::PEOPLE.sample)
+      w = w.gsub('[says]', Drama::Constants::SAYS.sample)
+      w = w.gsub('[badsoft]', Drama::Constants::BADSOFT.sample)
+      w = w.gsub('[function]', Drama::Constants::FUNCTIONS.sample)
+      w = w.gsub('[adj]', Drama::Constants::ADJECTIVES.sample)
+      w = w.gsub('[enormous]', Drama::Constants::SIZES.sample)
+      w = w.gsub('[price]', Drama::Constants::PRICES.sample)
+      w = w.gsub('[ac1]', Drama::Constants::BADVERBS.sample)
+      w = w.gsub('[packs]', Drama::Constants::PACKS.sample)
+      w = w.gsub('[drama]', Drama::Constants::DRAMA.sample)
+      w = w.gsub('[code]', Drama::Constants::CODE.sample)
+      w = w.gsub('[ban]', Drama::Constants::BANS.sample)
+      w = w.gsub('[activates]', Drama::Constants::ACTIVATES.sample)
       sent[num] = w
     end
 
