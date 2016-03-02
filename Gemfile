208256d8833db031b9c160b0c86cabecf93e1541
elifoster <elifosterwy@gmail.com>
2016-03-02 15:33:18 -0800

    :lock: Use StringUtility#safely_gsub! instead of gsub! for null safety. This has no significant performance hit, even though safely_gsub! is somewhat slower than gsub!, at MAYBE an extra 0.001s, but usually it takes the exact same amount of time to get the request.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 ruby '2.3.0'
 gem('sinatra', '1.4.6')
+gem('string-utility', '2.7.0')
 
 group :test do
   gem('rake')
--- lib/drama/sentence_helper.rb
+++ lib/drama/sentence_helper.rb
@@ -1,7 +1,10 @@
+require 'string-utility'
 require_relative 'constants'
 
 # The Sinatra helper for generating sentences.
 module SentenceHelper
+  using StringUtility
+
   # Generates a random dramatic sentence.
   # @return [String] The randomly generated sentence, with all "variables" removed.
   def generate_sentence
@@ -9,21 +12,21 @@ module SentenceHelper
     sent = Drama::Constants::SENTENCES.sample.split(/\s/)
     sent.each do |w|
       num = sent.index(w)
-      w.gsub!('[things]', Drama::Constants::THINGS.sample)
-      w.gsub!('[sites]', Drama::Constants::SITES.sample)
-      w.gsub!('[people]', Drama::Constants::PEOPLE.sample)
-      w.gsub!('[says]', Drama::Constants::SAYS.sample)
-      w.gsub!('[badsoft]', Drama::Constants::BADSOFT.sample)
-      w.gsub!('[function]', Drama::Constants::FUNCTIONS.sample)
-      w.gsub!('[adj]', Drama::Constants::ADJECTIVES.sample)
-      w.gsub!('[enormous]', Drama::Constants::SIZES.sample)
-      w.gsub!('[price]', Drama::Constants::PRICES.sample)
-      w.gsub!('[ac1]', Drama::Constants::BADVERBS.sample)
-      w.gsub!('[packs]', Drama::Constants::PACKS.sample)
-      w.gsub!('[drama]', Drama::Constants::DRAMA.sample)
-      w.gsub!('[code]', Drama::Constants::CODE.sample)
-      w.gsub!('[ban]', Drama::Constants::BANS.sample)
-      w.gsub!('[activates]', Drama::Constants::ACTIVATES.sample)
+      w.safely_gsub!('[things]', Drama::Constants::THINGS.sample)
+      w.safely_gsub!('[sites]', Drama::Constants::SITES.sample)
+      w.safely_gsub!('[people]', Drama::Constants::PEOPLE.sample)
+      w.safely_gsub!('[says]', Drama::Constants::SAYS.sample)
+      w.safely_gsub!('[badsoft]', Drama::Constants::BADSOFT.sample)
+      w.safely_gsub!('[function]', Drama::Constants::FUNCTIONS.sample)
+      w.safely_gsub!('[adj]', Drama::Constants::ADJECTIVES.sample)
+      w.safely_gsub!('[enormous]', Drama::Constants::SIZES.sample)
+      w.safely_gsub!('[price]', Drama::Constants::PRICES.sample)
+      w.safely_gsub!('[ac1]', Drama::Constants::BADVERBS.sample)
+      w.safely_gsub!('[packs]', Drama::Constants::PACKS.sample)
+      w.safely_gsub!('[drama]', Drama::Constants::DRAMA.sample)
+      w.safely_gsub!('[code]', Drama::Constants::CODE.sample)
+      w.safely_gsub!('[ban]', Drama::Constants::BANS.sample)
+      w.safely_gsub!('[activates]', Drama::Constants::ACTIVATES.sample)
       sent[num] = w
     end
 
