20b552df52db89b95c97a38db2eeb51414d3ce23
elifoster <elifosterwy@gmail.com>
2020-05-14 10:53:05 -0700

    :wrench: Use proper string formatting instead of a million ggsubs

--- lib/drama/sentence_helper.rb
+++ lib/drama/sentence_helper.rb
@@ -12,22 +12,23 @@ module SentenceHelper
     sent = Drama::Constants::SENTENCES.sample.split(/\s/)
     sent.each do |w|
       num = sent.index(w)
-      w.gsub!('[things]', Drama::Constants::THINGS.sample)
-      w.gsub!('[sites]', Drama::Constants::SITES.sample)
-      w.gsub!('[people]', Drama::Constants::PEOPLE.sample)
-      w.gsub!('[says]', Drama::Constants::SAYS.sample)
-      w.gsub!('[badsoft]', Drama::Constants::BADSOFT.sample)
-      w.gsub!('[function]', Drama::Constants::FUNCTIONS.sample)
-      w.gsub!('[adj]', Drama::Constants::ADJECTIVES.sample)
-      w.gsub!('[enormous]', Drama::Constants::SIZES.sample)
-      w.gsub!('[price]', Drama::Constants::PRICES.sample)
-      w.gsub!('[ac1]', Drama::Constants::BADVERBS.sample)
-      w.gsub!('[packs]', Drama::Constants::PACKS.sample)
-      w.gsub!('[drama]', Drama::Constants::DRAMA.sample)
-      w.gsub!('[code]', Drama::Constants::CODE.sample)
-      w.gsub!('[ban]', Drama::Constants::BANS.sample)
-      w.gsub!('[activates]', Drama::Constants::ACTIVATES.sample)
-      sent[num] = w
+      sent[num] = w % {
+        things: Drama::Constants::THINGS.sample,
+        sites: Drama::Constants::SITES.sample,
+        people: Drama::Constants::PEOPLE.sample,
+        says: Drama::Constants::SAYS.sample,
+        badsoft: Drama::Constants::BADSOFT.sample,
+        function: Drama::Constants::FUNCTIONS.sample,
+        adj: Drama::Constants::ADJECTIVES.sample,
+        enormous: Drama::Constants::SIZES.sample,
+        price: Drama::Constants::PRICES.sample,
+        ac1: Drama::Constants::SIZES.sample,
+        packs: Drama::Constants::PACKS.sample,
+        drama: Drama::Constants::DRAMA.sample,
+        code: Drama::Constants::CODE.sample,
+        ban: Drama::Constants::BANS.sample,
+        activates: Drama::Constants::ACTIVATES.sample
+      }
     end
 
     # add_a_drama
